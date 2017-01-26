2609672f5d358efdf0e53049d719fee9cb917283
Johannes Körner <johnniewalker.de@gmail.com>
2017-01-26 19:55:28 +0100

    Projekt angelegt

---
+++ AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace A1702dojoAufgabe.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// Allgemeine Informationen über eine Assembly werden über die folgende 
+// Attributgruppe gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[<assembly: AssemblyTitle("A1702dojoAufgabe")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("A1702dojoAufgabe")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Durch Festlegen von ComVisible auf 'false' werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[<assembly: ComVisible(false)>]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[<assembly: Guid("58de8db6-e26d-439a-94bd-099751c1b732")>]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+// 
+//       Hauptversion
+//       Nebenversion
+//       Buildnummer
+//       Revision
+// 
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [<Assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
